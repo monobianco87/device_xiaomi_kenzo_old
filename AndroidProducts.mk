267ce85d3466ae457711e5c031a74b6a3b9e87bd
mono <monobianco87@gmail.com>
2019-12-21 17:15:13 +0200

    Syberia OS bring up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_kenzo.mk
+    $(LOCAL_DIR)/syberia_kenzo.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_kenzo-userdebug \
-    aosp_kenzo-eng
+    syberia_kenzo-userdebug \
+    syberia_kenzo-eng
